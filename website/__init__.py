0d6a6479c3e59b6ea633383e39c1df9347a076cb
swapno-ahmed <ahmed.symum.swapno@g.bracu.ac.bd>
2022-04-12 21:02:19 +0600

    added instruction and basic codes to assessment, assesment_add, course

--- website/__init__.py
+++ website/__init__.py
@@ -27,6 +27,7 @@ def connect_database(app, db):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
     app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     app.config['MYSQL_DB'] = 'e_learning'
 
     db.init(app)
