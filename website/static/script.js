ffec6a6ae80a086a99d55ceb670439e99d33bf99
swapnilGhosh007 <66404911+swapnilGhosh007@users.noreply.github.com>
2022-04-20 03:40:02 +0600

    implemented the feedback page and designed and done something with the content page

--- website/static/script.js
+++ website/static/script.js
@@ -5,3 +5,13 @@ function openForm(id) {
 function closeForm(id) {
   document.getElementById(id).style.display = "none";
 }
+
+const container = document.querySelector(".feedback_container"),
+      privacy = container.querySelector(".feedback_post .feedback_privacy"),
+      arrowBack = container.querySelector(".feedback_audience .feedback_arrow-back");
+      privacy.addEventListener("click", () => {
+        container.classList.add("active");
+      });
+      arrowBack.addEventListener("click", () => {
+        container.classList.remove("active");
+      });
